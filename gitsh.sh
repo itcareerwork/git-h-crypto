090f5b6e8b1fa0b9d6326d7668d0de84e047f35a
Dmitry Bezmelnitsin <itcareerwork@gmail.com>
2020-11-10 03:26:58 +0500

    add colors V1

--- gitsh.sh
+++ gitsh.sh
@@ -14,14 +14,65 @@ language_ru=("Русский" "Добавить все/коммит")
 
 lng="language_$lng[@]"; lng=("${!lng}")
 
+#&& sleep 2
 
-pwds() {
-	echo ""
-	echo ---------
-	echo -e "\[\e[5m $PWD \e[0m"
-	echo ---------
+colors() {
+	
+	clear
+	let backgr=16
+	let txts=15
+ 	for backgr in {0..255}; do
+		if (( $backgr % 16 == 0 )); then 
+			echo -e ""
+		fi
+
+		if (( "$backgr" < 10 )); then
+			echo -en " \e[38;5;$txts;48;5;$backgr;1m$txts;$backgr\e[0m"
+		elif (( "$backgr" < 100 )); then
+			echo -en " \e[38;5;$txts;48;5;$backgr;1m$txts;$backgr\e[0m"
+		else
+			echo -en " \e[38;5;$txts;48;5;$backgr;1m$txts;$backgr\e[0m"
+		fi
+
+		(( i++ ))
+	 done
+echo
+expr length "$txts;$backgr"
 }
 
+
+pwds() {
+
+echo ""
+
+
+
+	echo ---------
+
+	echo -en "$PWD"
+	sleep 0.1  
+	echo -en "\r\e[2K\e[7m$PWD"
+	sleep 0.1 
+	echo -en "\r\e[2K\e[27m$PWD"
+	sleep 0.1 
+	echo -en "\r\e[2K\e[7m$PWD"
+	sleep 0.1 
+	echo -en  "\r\e[2K\e[27m$PWD"
+	sleep 0.1 
+	echo -en "\r\e[0K\e[32m[$(date +"%T")]\e[0;1m"
+	echo -e  "\e[48;5;24;38;5;15m${PWD%/*}/\e[0;1;33m$(basename "$PWD")\e[0m"
+
+	
+
+	 echo ---------
+colors
+
+# 23,24,65,66,71,101,102
+
+}
+#rc=E8,:  восстановление позиции курсора
+
+#sc=E7,:  сохранение положения курсора##
 
 main() {
 	menu0=("menu0" "${lng[1]};gitadd" "Git push;gitpush" "Git create tag;gittag" "Git select proj;gitselect" "Git init;gitinit" "Git clone;gitclone" "New Git project;gitnew")
